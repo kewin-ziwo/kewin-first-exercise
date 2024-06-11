b01473f096a686edfb4204cc8d5fe13e5cf11fad
kewin-ziwo <kewin.fernando@ziwo.io>
2024-06-11 18:17:53 +0400

    nav-link component added

--- src/components/navigation-bar/navigation-bar.component.ts
+++ src/components/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NavLinkComponent } from "../nav-link/nav-link.component";
 
 @Component({
   selector: "app-navigation-bar",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavLinkComponent],
   templateUrl: "./navigation-bar.component.html",
   styleUrl: "./navigation-bar.component.css",
 })
--- src/components/sub-navigation-bar/sub-navigation-bar.component.ts
+++ src/components/sub-navigation-bar/sub-navigation-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ButtonComponent } from "../button/button.component";
+import { NavLinkComponent } from "../nav-link/nav-link.component";
 
 @Component({
   selector: "app-sub-navigation-bar",
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, NavLinkComponent],
   templateUrl: "./sub-navigation-bar.component.html",
   styleUrl: "./sub-navigation-bar.component.css",
 })
