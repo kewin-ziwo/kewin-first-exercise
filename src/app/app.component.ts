e4335be20385b159a58f557124cb7a7f0365d2de
kewin-ziwo <kewin.fernando@ziwo.io>
2024-06-11 17:50:14 +0400

    button component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { ContentWrapperComponent } from 'src/components/main-nav/content-wrapper.component';
-import { NavigationBarComponent } from 'src/components/navigation-bar/navigation-bar.component';
-import { SubNavigationBarComponent } from 'src/components/sub-navigation-bar/sub-navigation-bar.component';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NxWelcomeComponent } from "./nx-welcome.component";
+import { ContentWrapperComponent } from "src/components/main-nav/content-wrapper.component";
+import { NavigationBarComponent } from "src/components/navigation-bar/navigation-bar.component";
+import { SubNavigationBarComponent } from "src/components/sub-navigation-bar/sub-navigation-bar.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ContentWrapperComponent, NavigationBarComponent, SubNavigationBarComponent],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [
+    RouterModule,
+    NxWelcomeComponent,
+    ContentWrapperComponent,
+    NavigationBarComponent,
+    SubNavigationBarComponent,
+  ],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'myngapp';
+  title = "myngapp";
 }
--- src/components/sub-navigation-bar/sub-navigation-bar.component.ts
+++ src/components/sub-navigation-bar/sub-navigation-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: "app-sub-navigation-bar",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: "./sub-navigation-bar.component.html",
   styleUrl: "./sub-navigation-bar.component.css",
 })
